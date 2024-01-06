b13338726f98568bfd7f80625549da6a092f8505
Mizko Danylo <mizko2000y@gmail.com>
2024-01-06 12:33:38 +0200

    FixLobby

---
+++ Assets/RoomManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+using System.IO; // дозволяє працювати із файлами(копіювати,переміщувати,відкривати і т.д)
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    public static RoomManager Instance;
+    private void Awake()
+    {
+        if(Instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+        Instance = this;
+    }
+    public override void OnEnable()
+    {
+        base.OnEnable();
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+    public override void OnDisable()
+    {
+        base.OnDisable();
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+    private void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
+    {
+        if(scene.buildIndex >= 1)
+        {
+            PhotonNetwork.Instantiate(Path.Combine("PlayerManager"), Vector3.zero, Quaternion.identity);
+        }
+    }
+
+}
